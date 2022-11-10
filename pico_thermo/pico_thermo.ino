d3f661170686ee67289f63e5b0a8c4b9eca89ea7
FlyingSheeps <misorlkoko@gmail.com>
2022-11-10 14:16:35 +0900

    add arduino code

---
+++ pico_thermo/pico_thermo.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>
+#include "Adafruit_ADT7410.h" //you need this library
+
+// Create the ADT7410 temperature sensor object
+Adafruit_ADT7410 tempsensor = Adafruit_ADT7410();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(1000);
+  
+  Wire.setSDA(20);
+  Wire.setSCL(21);
+  Wire.begin();
+  
+    if (!tempsensor.begin()) {
+   
+    while (1){ 
+      Serial.println("Couldn't find ADT7410!");
+      delay(1000);
+      };
+  }
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float c = tempsensor.readTempC();
+  Serial.println(c);
+  delay(1000);
+
+}
