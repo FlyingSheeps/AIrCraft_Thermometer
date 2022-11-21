1f04fafddcd0d8d0dc17c092234e91a65cb12697
FlyingSheeps <misorlkoko@gmail.com>
2022-11-21 21:19:37 +0900

    update readme

--- Simpleadaptive.m
+++ Simpleadaptive.m
@@ -1,8 +1,11 @@
 %炉の状態空間表現
-a = -0.01 %時定数
-b = 0.05 %適当な入力係数
+a = -1 %時定数
+b = 1 %適当な入力係数
 ro = ss(a,b,1,0)
-%stepplot(ro)
+figure
+stepplot(ro)
+plt.linewidth=2
+title('対象モデル（一次遅れ）のステップ応答')
 
 %シム時間
 d = 0.01
@@ -16,8 +19,8 @@ e  = zeros(size(t));
 Tr = [t(1:300000)./100.*1.3 ones(1,300000).*3000./100.*1.3 t(300000:700000)./100.*1.3];
 
 %調整パラメータ
-Gamma = diag([1000 0.00]);
-sigma = diag([1 1]);
+Gamma = diag([1000 0.00]); %適応のゲイン
+sigma = diag([1 1]); %フィルタのカットオフ角振動数に相当
 
 %シム
 for n = 1:size(t,2)-1
@@ -27,7 +30,10 @@ for n = 1:size(t,2)-1
 end
 figure
 plot(t,[dT ; Tr],linewidth=2)
+title('温度（青）と目標温度（橙）')
 figure
 plot(t,KI,linewidth=2)
+title('ゲインプロット')
 figure
 plot(t,e,linewidth=2)
+title('誤差プロット')
